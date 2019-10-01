fcf0487354db04cbda28c23025685a69d9decdce
Dan Gopstein <dan@gopstein.com>
2019-10-01 16:25:13 -0400

    Fix flake8 import error

--- swe1/settings.py
+++ swe1/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -113,8 +114,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = "/static/"
-
-# Configure Django App for Heroku.
-import django_heroku
 
 django_heroku.settings(locals(), test_runner=False)

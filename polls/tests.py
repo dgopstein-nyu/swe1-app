a95d0c3777a1ca133b8f231c5f6f150031c93a40
gcivil <53953892+gcivil-nyu@users.noreply.github.com>
2019-10-10 17:20:11 -0400

    test again

--- polls/tests.py
+++ polls/tests.py
@@ -4,6 +4,7 @@ from django.test import TestCase
 from django.utils import timezone
 from django.urls import reverse
 from django.urls import reverse
+from django,urls import reverse
 
 from .models import Question
 
